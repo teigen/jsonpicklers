06a47bcd1e4f2ac4a7f5255c155b6b2b719bd248
Ole Christian Rynning <oc@rynning.no>
2012-10-29 13:39:09 +0100

    Improved API examples with inline examples.

--- src/test/scala/simple/APIExamples.scala
+++ src/test/scala/simple/APIExamples.scala
@@ -1,26 +1,74 @@
 package simple
 
+import org.scalatest._
+import org.scalatest.matchers._
 import jsonpicklers.Picklers._
-import java.net.URL
+import net.liftweb.json._
 
-object Hello {
-  val json = wrap(apply)(unapply(_).get){
-    "message" :: string
-  }
-}
-case class Hello(message:String)
+class ApiExamples extends FreeSpec with ShouldMatchers {
 
-object Sizes {
-  val json = wrap(apply)(unapply(_).get){
-    ("sizes" :: ( * :: Size.json))
-  }
-}
-case class Sizes(shapes:Map[String,Size])
+  "Supports simple values (Hello(\"world\"))" - {
 
-object Size {
-  val json = wrap(apply)(unapply(_).get){
-    ("x" :: int) ~
-    ("y" :: int)
+    object Hello {
+      val json = wrap(apply)(unapply(_).get){
+        "message" :: string
+      }
+    }
+    case class Hello(message:String)
+
+    val json = parse("""{ "message": "world" }""")
+    val scala = Hello("world")
+
+    "pickles" in {
+      Hello.json.pickle(scala) should equal(json)
+    }
+
+    "unpickles" in {
+      Hello.json.unpickle(json) match {
+        case jsonpicklers.Success(value, _) => value should equal(scala)
+        case f => fail(f.toString)
+      }
+    }
   }
+
+  "Supports maps of named values (Map[String,Obj])" - {
+    object Size {
+      val json = wrap(apply)(unapply(_).get){
+        ("x" :: int) ~
+          ("y" :: int)
+      }
+    }
+    case class Size(x:Int, y:Int)
+
+    object Sizes {
+      val json = wrap(apply)(unapply(_).get){
+        ("sizes" :: ( * :: Size.json))
+      }
+    }
+    case class Sizes(shapes:Map[String,Size])
+
+    val json = parse("""
+                    {
+                      "sizes" : {
+                        "small": { "x": 200, "y": 200 },
+                        "medium":{ "x": 400, "y": 400 },
+                        "large": { "x": 600, "y": 600 }
+                      }
+                    }
+                    """)
+
+    val scala = Sizes(Map("small" -> Size(200, 200), "medium" -> Size(400, 400), "large" -> Size(600, 600)))
+
+    "pickles" in {
+      Sizes.json.pickle(scala) should equal(json)
+    }
+
+    "unpickles" in {
+      Sizes.json.unpickle(json) match {
+        case jsonpicklers.Success(value, _) => value should equal(scala)
+        case f => fail(f.toString)
+      }
+    }
+  }
+
 }
-case class Size(x:Int, y:Int)
